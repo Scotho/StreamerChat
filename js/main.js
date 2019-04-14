d61c5685daa8e5b18c527a83c7c9f25c61c72b2f
Scotho <crsmithss@gmail.com>
2019-04-13 23:41:23 -0300

    Testing handleChat

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 var channels = ['moonmoon_ow']; // Channels to initially join
 
-fetch('https://api.betterttv.net/2/channels/'+channels[0])
-.then(json => json.json())
-.then(data => {
-    console.log(data);
-    if (!data.status || data.status != 404) {
-        for (var index = 0; index < data.emotes.length; index++) {
-            var emote = data.emotes[index];
-            bttvEmotes[emote.code] = emote.id;
-        }
-    }
-})
-
 var clientOptions = {
         options: {
                 debug: false
@@ -29,17 +17,14 @@ function dehash(channel) {
 }
 
 function handleChat(channel, user, message, self) {
-    //console.log(user);
-    /*if (message.length < 20 && user.badges && user.badges.subscriber && user.badges.subscriber !== '0' && Math.random() > 0.9) {
-        ctx.fillStyle = '#fff';
-        ctx.globalAlpha = 1;
-        ctx.font = 'bold 100px monospace';
-        ctx.fillText(message, canvas.width/2.5, canvas.height - canvas.height/10, canvas.width/2);
-    }*/
     var chan = dehash(channel),
         name = user.username;
-
+    console.log('---------');
     console.log(user + ": " + message);
+    console.log(self);
+    console.log(channel);
+    console.log('---------');
+
 }
 
 client.addListener('message', handleChat);
